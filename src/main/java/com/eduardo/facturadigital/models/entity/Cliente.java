1b84fc49baef192c04160ee1ad954454b49a0cbd
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-23 16:00:47 -0600

    added: Cliente.java

---
+++ src/main/java/com/eduardo/facturadigital/models/entity/Cliente.java
@@ -0,0 +1,48 @@
+package com.eduardo.facturadigital.models.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Data
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
+    @NotEmpty
+    @Email
+    private String email;
+    @NotNull
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createAt;
+
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Factura> facturas;
+
+    private String foto;
+
+    public void addFactura(Factura factura){
+        factura.add(factura);
+    }
+
+    public static long getSerialversionuid(){
+        return serialVersionUID;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/eduardo/facturadigital/models/entity/Factura.java
@@ -0,0 +1,4 @@
+package com.eduardo.facturadigital.models.entity;
+
+public class Factura {
+}
