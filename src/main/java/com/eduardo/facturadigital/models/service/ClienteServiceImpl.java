ea833a00dd3fba7d66123c7dae08ec79c239c7bc
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-24 08:09:58 -0600

    fixed: IClienteDao

--- src/main/java/com/eduardo/facturadigital/models/service/ClienteServiceImpl.java
+++ src/main/java/com/eduardo/facturadigital/models/service/ClienteServiceImpl.java
@@ -1,4 +1,46 @@
 package com.eduardo.facturadigital.models.service;
 
-public class ClienteServiceImpl {
+import com.eduardo.facturadigital.models.dao.IClienteDao;
+import com.eduardo.facturadigital.models.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements IClienteService{
+
+    @Autowired
+    private IClienteDao clienteDao;
+
+    @Override
+    public List<Cliente> findAll() {
+        // TODO Auto-generated method stub
+        return (List<Cliente>) clienteDao.findAll();
+    }
+
+    @Override
+    public Page<Cliente> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public void save(Cliente cliente) {
+        clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findOne(Long id) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+
+    }
 }
