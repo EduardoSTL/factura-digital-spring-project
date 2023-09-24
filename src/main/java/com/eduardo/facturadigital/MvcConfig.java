afe90c09b0d8c19a5c86ef12dbcfbb2b7c56b785
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-24 10:14:52 -0600

    updt: MvcConfig.java

--- src/main/java/com/eduardo/facturadigital/MvcConfig.java
+++ src/main/java/com/eduardo/facturadigital/MvcConfig.java
@@ -1,8 +1,10 @@
 package com.eduardo.facturadigital;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
